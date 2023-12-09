b17b5b9f65916312143f0150468cd3997128d2ef
Marcos <marcsalexandr@gmail.com>
2023-12-08 22:34:48 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+
+  @Input()
+  photoCover:string="https://s2-techtudo.glbimg.com/jBWRLpzFfnoaaDUgIUMJTTORCyM=/0x0:896x505/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2019/T/a/A0BpXZRG2ERko5MF8gJw/captura-de-tela-2019-03-19-as-13.56.29.png"
+  cardTitle:string="A emoção do Call of Duty em movimento"
+  cardDescription:string="Call of Duty: Mobile is a free-to-play first-person shooter video game developed by Tencent Games and published by Activision for Android and iOS. It was released on October 1, 2019. The game received over 20 million downloads within a day of its release, earning over $2 million in revenue."
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string ="https://overplay.com.br/wp-content/uploads/2021/06/cod-mobile-2.jpg"
+  @Input()
+  cardTitle:string="Crossfire"
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
